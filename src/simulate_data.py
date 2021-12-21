a21fbcc1d38c6e1b432d966bb1a05997d34bd7c7
Gustav Rasmussen <gcr84@hotmail.com>
2021-12-21 11:37:31 +0100

    Update data simulator

--- src/simulate_data.py
+++ src/simulate_data.py
@@ -6,6 +6,9 @@ Purpose: Simulate weight-training data
 
 import json
 import pathlib
+import random
+
+# from pprint import pprint as pp
 
 from faker import Faker
 from faker.providers import DynamicProvider
@@ -13,16 +16,24 @@ from faker.providers import DynamicProvider
 fake = Faker()
 
 
-def simulate_data():
-    """Simulate data for workout dates, splits and exercises."""
+def simulate_date_and_split():
+    """Simulate data for workout date and split."""
 
-    workout_dates = [fake.date() for _ in range(10)]
+    workout_date = fake.date()
 
     splits_provider = DynamicProvider(
         provider_name="splits",
         elements=["back", "chest", "legs", "shoulders"],
     )
 
+    fake.add_provider(splits_provider)
+    workout_split = fake.splits()
+
+    return workout_date, workout_split
+
+
+def simulate_exercises(workout_split):
+    """Simulate data for exercises."""
     chest_exercises_provider = DynamicProvider(
         provider_name="chest_exercises",
         elements=["benchpress", "flys", "pullovers", "dips"],
@@ -48,88 +59,61 @@ def simulate_data():
         ],
     )
 
-    fake.add_provider(splits_provider)
     fake.add_provider(chest_exercises_provider)
     fake.add_provider(back_exercises_provider)
     fake.add_provider(leg_exercises_provider)
     fake.add_provider(shoulder_exercises_provider)
 
-    workout_splits = [fake.splits() for _ in range(10)]
     workout_chest_exercises = [fake.unique.chest_exercises() for _ in range(3)]
     workout_back_exercises = [fake.unique.back_exercises() for _ in range(3)]
     workout_leg_exercises = [fake.unique.leg_exercises() for _ in range(3)]
     workout_shoulder_exercises = [fake.unique.shoulder_exercises() for _ in range(3)]
 
-    # for wd, ws in zip(workout_dates, workout_splits):
-    #     print(wd, ws)
-
-    """
-    for ce, be, le, se in zip(
-        workout_chest_exercises,
-        workout_back_exercises,
-        workout_leg_exercises,
-        workout_shoulder_exercises,
-    ):
-        print(ce, be, le, se)
-    """
-    return (
-        workout_dates,
-        workout_splits,
-        workout_chest_exercises,
-        workout_back_exercises,
-        workout_leg_exercises,
-        workout_shoulder_exercises,
-    )
-
-
-def format_data(sim_data):
-    """Prepare data format for JSON file."""
-
-    (
-        workout_dates,
-        workout_splits,
-        workout_chest_exercises,
-        workout_back_exercises,
-        workout_leg_exercises,
-        workout_shoulder_exercises,
-    ) = sim_data
-
-    date = workout_dates[0]
-    split = workout_splits[0]
-
-    split_exercises_map = {
-        "chest": workout_chest_exercises,
+    switcher = {
         "back": workout_back_exercises,
+        "chest": workout_chest_exercises,
         "legs": workout_leg_exercises,
         "shoulders": workout_shoulder_exercises,
     }
 
-    exercises = split_exercises_map[split]
-    # print(split, exercises)
+    return switcher.get(workout_split, "Invalid muscle-group")
 
-    data = {
-        "date": date,
-        "split": workout_splits[0],
-        "exercises": {
-            exercises[0]: [
-                {"set no.": 1, "reps": "null", "weight": "value kg"},
-                {"set no.": 2, "reps": "null", "weight": "value kg"},
-                {"set no.": 3, "reps": "null", "weight": "value kg"},
-            ],
-            exercises[1]: [
-                {"set no.": 1, "reps": "null", "weight": "value kg"},
-                {"set no.": 2, "reps": "null", "weight": "value kg"},
-                {"set no.": 3, "reps": "null", "weight": "value kg"},
-            ],
-            exercises[2]: [
-                {"set no.": 1, "reps": "null", "weight": "value kg"},
-                {"set no.": 2, "reps": "null", "weight": "value kg"},
-                {"set no.": 3, "reps": "null", "weight": "value kg"},
-            ],
-        },
-    }
+
+def simulate_sets_reps_weight():
+    """Simulate data for sets, reps and weight."""
+    no_of_sets = random.randint(1, 6)
+
+    data = []
+
+    for set in range(1, no_of_sets + 1):
+        data.append(
+            {
+                "set no.": set,
+                "reps": random.randint(1, 20),
+                "weight": f"{random.randint(1, 100)} kg",
+            }
+        )
 
     return data
+
+
+def attach_data_to_exercise(exercises):
+    """Fill in sets, reps and weight data for each exercise"""
+
+    data = {}
+
+    for exercise in exercises:
+        data[exercise] = simulate_sets_reps_weight()
+
+    return data
+
+
+def format_data(workout_date, workout_split, data):
+    """Prepare data format for JSON file."""
+
+    final_data = {"date": workout_date, "split": workout_split, "exercises": data}
+
+    return final_data
 
 
 def write_data(formatted_data):
@@ -147,10 +131,17 @@ def write_data(formatted_data):
 
 
 def main():
-    """Simulate, format and insert data into JSON file."""
-    sim_data = simulate_data()
-    formatted_data = format_data(sim_data)
-    write_data(formatted_data)
+    """Simulate specified number of workouts and insert their data into JSON files."""
+
+    number_of_workouts = 2
+
+    for workout in range(number_of_workouts):
+        workout_date, workout_split = simulate_date_and_split()
+        exercises = simulate_exercises(workout_split)
+        data = attach_data_to_exercise(exercises)
+        formatted_data = format_data(workout_date, workout_split, data)
+        # pp(formatted_data)
+        write_data(formatted_data)
 
 
 if __name__ == "__main__":

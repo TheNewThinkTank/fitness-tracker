15e51234b16185d32518f14936629524c3f68b4e
Gustav Rasmussen <gcr84@hotmail.com>
2021-12-30 20:08:06 +0100

    Add parametrized unit tests for simulations

--- test/test_simulation.py
+++ test/test_simulation.py
@@ -2,8 +2,7 @@
 unit test suite for src folder
 """
 
-# from tinydb import TinyDB
-# import pytest
+import pytest
 
 import sys
 import os
@@ -11,16 +10,43 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from src.simulations.simulate_data import get_available_exercises
+from src.simulations.simulate_data import SimulateWorkout
 
 
-def test_get_available_exercises():
-    """Verify that get_available_exercises includes required exercises for given muscle group."""
-    assert all(
-        x in get_available_exercises()
-        for x in [
-            {"barbell bench press": [50, 90]},
-            {"dumbbell fly": [10, 18]},
-            {"barbell incline press": [30, 70]},
-        ]
-    )
+@pytest.mark.parametrize(
+    "test_input_reps,expected",
+    [
+        (1, "9.75 kg"),
+        (2, "9.50 kg"),
+        (3, "9.25 kg"),
+        (4, "9.00 kg"),
+        (5, "8.75 kg"),
+        (6, "8.50 kg"),
+        (7, "8.25 kg"),
+        (8, "8.00 kg"),
+        (9, "7.75 kg"),
+        (10, "7.50 kg"),
+    ],
+)
+def test_high_reps_low_weight(test_input_reps, expected):
+    """Verify that high_reps_low_weight gives lower weight for higher reps."""
+    simulated_workout = SimulateWorkout("2021-01-01", 1)
+    weight_choice = simulated_workout.high_reps_low_weight([2, 10], test_input_reps)
+    assert weight_choice == expected
+
+
+@pytest.mark.parametrize(
+    "test_input_progression,expected",
+    [
+        (1, "8.75 kg"),
+        (1.01, "8.84 kg"),
+        (1.02, "8.93 kg"),
+        (1.03, "9.01 kg"),
+        (1.04, "9.10 kg"),
+    ],
+)
+def test_progression(test_input_progression, expected):
+    """Verify that progression factor estimates weights correctly"""
+    simulated_workout = SimulateWorkout("2021-01-01", test_input_progression)
+    weight_choice = simulated_workout.high_reps_low_weight([2, 10], 5)
+    assert weight_choice == expected

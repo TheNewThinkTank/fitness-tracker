f80dc41278d629a89c1090a1f633d3a1140dfb72
Gustav Rasmussen <gcr84@hotmail.com>
2021-12-31 16:19:29 +0100

    Add unit test of model

--- test/test_model.py
+++ test/test_model.py
@@ -10,10 +10,9 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from src.model.model import get_df  # , one_rep_max_estimator
+from src.model.model import get_df, one_rep_max_estimator
 
 from tinydb import TinyDB
-
 
 db = TinyDB("../data/sim_db.json")
 # db = TinyDB("data/sim_db.json")
@@ -25,10 +24,13 @@ table = db.table("weight_training_log")
     [("legs", "squat")],  # , ('chest', 'pullover')],
 )
 def test_get_df(test_input_split, test_input_exercise):
-    """Verify that blabla."""
-    assert len(get_df(table, test_input_split, test_input_exercise)) > 2
+    """Verify that dataframe has more than 2 entries."""
+    df = get_df(table, test_input_split, test_input_exercise)
+    assert len(df) > 2
 
 
-# def test_one_rep_max_estimator(df):
-#     """Verify that blabla."""
-#     pass
+def test_one_rep_max_estimator():
+    """Verify that 1RM has progression."""
+    df = get_df(table, "legs", "squat")
+    df_1rm = one_rep_max_estimator(df)
+    assert df_1rm.loc["2019-01-04", "1RM"] <= df_1rm.loc["2019-01-09", "1RM"]

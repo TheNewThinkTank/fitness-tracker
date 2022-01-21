dad3df3931a853df80ef2b550c5c126d200cca51
Gustav Rasmussen <gcr84@hotmail.com>
2022-01-21 20:36:48 +0100

    Add bash flow script for real workout data

---
+++ src/real_flow.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/bash
+
+# Date: 2022-01-21
+# Author: Gustav Collin Rasmussen
+# Purpose: BASH workflow that inserts data in database.
+
+python3 src/CRUD/insert.py real 2022-01-21
+echo 'data inserted in database'  # . Preparing figures ..'
+# python3 src/model/plot_model.py real
+# open img/fitted_data_barbell_bench_press.png

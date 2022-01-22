850b484f53011fc9de9cbc7581fc72ba255eec30
Gustav Rasmussen <gcr84@hotmail.com>
2022-01-22 17:08:17 +0100

    Cleanup r script

--- src/1rm.r
+++ src/1rm.r
@@ -92,30 +92,3 @@ sp4 <- ggplot(df4, aes(x = reps, y = inverse_one_rep_max)) +
 plot_grid(sp1, sp2, sp3, sp4,
           labels = rep(c("constant reps (5)", "constant weight (70 kg)"), times=2),
           ncol = 2, nrow = 2)
-
-# weights = c(10, 20, 30, 40, 50)
-# weights = seq(10, 50, by=10)
-# print(weights)
-# reps = c(1, 2, 3, 4, 5)
-# reps = 2:6
-# print(reps)
-
-# progressions = seq(10, 100, by=10)
-# for (progression in progressions){
-#     print(epley_inverted(58.33333, 5, progression))
-# }
-
-# for (rep in reps){
-    # print(epley_inverted(58.33333, rep))
-    # print(brzycki_inverted(56.25, rep))
-# }
-
-# epley_1rm = epley(50, 5)
-# brzycki_1rm = brzycki(50, 5)
-# print(epley_1rm)
-# print(brzycki_1rm)
-
-# epley_weight = epley_inverted(58.33333, 5)
-# brzycki_weight = brzycki_inverted(56.25, 5)
-# print(epley_weight)
-# print(brzycki_weight)

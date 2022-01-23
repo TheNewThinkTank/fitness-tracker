b1966feb9df7c8b72c5bbda6793f9b4b13c4ad43
Gustav Rasmussen <gcr84@hotmail.com>
2022-01-23 18:29:19 +0100

    Add today's workout

--- src/model/plot_model.py
+++ src/model/plot_model.py
@@ -19,10 +19,9 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 from model import get_data, get_df, one_rep_max_estimator
 
 
-def create_plots(x, y, exercise):
+def create_plots(datatype, x, y, exercise):
     """Plot training data with fit"""
 
-    # plt.clf()
     plt.figure(figsize=(8, 8))
 
     # Only add confidence intervals if there are sufficient data points
@@ -36,44 +35,46 @@ def create_plots(x, y, exercise):
 
     xticks = ax.get_xticks()
     xticks_dates = [datetime.fromtimestamp(x).strftime("%Y-%m-%d") for x in xticks]
-    # pp(xticks_dates)
+
     ax.set_xticklabels(xticks_dates)
     plt.ylim(0, max(y) + 5)
-    # plt.xlim(min(x), max(x))
-    # ax.set_xlabel("workout date")
     plt.xticks(rotation=45)
     ax.set_ylabel(f"1 RM estimates [kg]")
-    # plt.show()
-    # print(x, y)
-    plt.savefig(f"img/fitted_data_{exercise}.png")
+    plt.savefig(f"img/{datatype}_fitted_data_{exercise}.png")
     plt.clf()  # clear figure before next plot
 
 
 def main():
     """Get data and create figure."""
 
-    # datatypes = ["real", "simulated"]
-    datatype = sys.argv[1]  # "real"  # datatypes[1]
+    datatype = sys.argv[1]  # real/simulated
     db = TinyDB("data/db.json") if datatype == "real" else TinyDB("data/sim_db.json")
     table = db.table("weight_training_log")
 
-    splits_and_key_exercises = [
-        ("chest", "barbell_bench_press"),
-        # ("back_and_biceps", "seated_row"),
-        # ("back", "seated_row"),
-        ("legs", "squat"),
-        ("legs", "deadlift"),
-        # ("legs", "legpress"),
-    ]
+    splits_and_key_exercises = (
+        [
+            ("chest", "barbell_bench_press"),
+            ("back_and_biceps", "diverging_seated_row"),
+            ("legs", "squat"),
+            ("legs", "deadlift"),
+            ("legs", "legpress"),
+        ]
+        if datatype == "real"
+        else [
+            ("chest", "barbell_bench_press"),
+            ("back", "seated_row"),
+            ("legs", "squat"),
+            ("legs", "deadlift"),
+            ("legs", "legpress"),
+        ]
+    )
 
     for split, exercise in splits_and_key_exercises:
 
         df = get_df(table, split, exercise)
         df_1rm = one_rep_max_estimator(df)
         x, y = get_data(df_1rm)
-        # print(x, y)
-        assert len(x) == len(y)
-        create_plots(x, y, exercise)
+        create_plots(datatype, x, y, exercise)
 
 
 if __name__ == "__main__":
--- src/real_flow.sh
+++ src/real_flow.sh
@@ -4,7 +4,8 @@
 # Author: Gustav Collin Rasmussen
 # Purpose: BASH workflow that inserts data in database.
 
-python3 src/CRUD/insert.py real 2022-01-21
-echo 'data inserted in database'  # . Preparing figures ..'
-# python3 src/model/plot_model.py real
-# open img/fitted_data_barbell_bench_press.png
+python3 src/CRUD/insert.py real 2022-01-23
+echo 'data inserted in database. Preparing figures ..'
+python3 src/model/plot_model.py real
+open img/real_fitted_data_diverging_seated_row.png
+# open img/real_fitted_data_barbell_bench_press.png

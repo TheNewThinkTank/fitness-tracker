846260637ca315a33048771b8ae638ecff0a22d3
Gustav Rasmussen <gcr84@hotmail.com>
2022-01-25 11:00:24 +0100

    Add today's workout

--- src/real_flow.sh
+++ src/real_flow.sh
@@ -4,8 +4,8 @@
 # Author: Gustav Collin Rasmussen
 # Purpose: BASH workflow that inserts data in database.
 
-python3 src/CRUD/insert.py real 2022-01-23
-echo 'data inserted in database. Preparing figures ..'
-python3 src/model/plot_model.py real
-open img/real_fitted_data_diverging_seated_row.png
+python3 src/CRUD/insert.py real 2022-01-25
+echo 'data inserted in database'  # . Preparing figures ..'
+# python3 src/model/plot_model.py real
+# open img/real_fitted_data_diverging_seated_row.png
 # open img/real_fitted_data_barbell_bench_press.png

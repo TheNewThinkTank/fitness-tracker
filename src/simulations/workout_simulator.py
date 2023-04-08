2c5bd48dffef2dbdf456bf26594459860f55d988
Gustav Rasmussen <gcr84@hotmail.com>
2023-04-08 19:07:40 +0200

    chore: refactor simulate_workout_data into two methods

--- src/simulations/workout_simulator.py
+++ src/simulations/workout_simulator.py
@@ -10,9 +10,25 @@ import numpy as np
 class WorkoutSimulator:
     """Simulate a workout"""
 
-    # available_splits = ["back", "chest", "legs", "shoulders"] # Define list of available muscle groups
-
     def __init__(self, workout_date: str, progress: int, training_catalogue: str, output_dir: str) -> None:
+        """
+        :param workout_date: Date of the workout in "YYYY-MM-DD" format
+        :param progress: Progress made since the last workout
+        :param training_catalogue: Path to YAML file with available exercises and weight ranges
+        :param output_dir: Path to directory to output the simulated workout JSON file
+        """
+        # Validate input
+        try:
+            assert len(workout_date) == 10
+            int(workout_date[:4])
+            int(workout_date[5:7])
+            int(workout_date[8:10])
+        except AssertionError:
+            raise ValueError("Invalid workout date")
+        if progress < 0:
+            raise ValueError("Progress must be greater than or equal to zero")
+        
+        
         self.workout_date: str = workout_date
         self.progress: int = progress
         self.training_catalogue: str = training_catalogue
@@ -23,7 +39,7 @@ class WorkoutSimulator:
         """Fetch musclegroup-exercises catalogue, with weight-ranges.
 
         :return: List of available exercises for the given muscle group
-        :rtype: list[dict]
+        :rtype: list[dict[str, list[int]]]
         """
 
         with open(self.training_catalogue, "r") as rf:
@@ -31,12 +47,11 @@ class WorkoutSimulator:
 
         return available_exercises[split]
 
-
     def simulate_exercises(self) -> list[dict[str, list[int]]]:
         """Simulate data for exercises.
 
         :return: _description_
-        :rtype: list[dict]
+        :rtype: list[dict[str, list[int]]]
         """
 
         available_exercises: list[dict[str, list[int]]] = self.get_available_exercises(self.split)
@@ -63,30 +78,47 @@ class WorkoutSimulator:
         # print(weight_range, actual_reps, self.progress, weight_choice)
         return f"{weight_choice:.2f} kg"
 
-    def simulate_sets_reps_weight(self) -> dict:
-        """Simulate data for sets, reps and weight.
+    def generate_exercise_mapping(self) -> dict:
+        """Generate a mapping of exercises to weight ranges.
 
-        :return: _description_
+        :return: A dictionary mapping exercise names to weight ranges.
         :rtype: dict
         """
 
-        mapping: dict = {}
+        exercise_mapping = {}
         for exercise in self.simulate_exercises():
-            no_of_sets = random.randint(1, 6)
-            for k, weight_range in exercise.items():
-                mapping[k] = []
-                for actual_set in range(1, no_of_sets + 1):
-                    actual_reps = random.randint(1, 10)
-                    actual_weight = self.calculate_weight(weight_range, actual_reps)
-                    mapping[k].append(
-                        {
-                            "set_number": actual_set,
-                            "reps": actual_reps,
-                            "weight": actual_weight,
-                        }
-                    )
+            for exercise_name, weight_range in exercise.items():
+                exercise_mapping[exercise_name] = weight_range
 
-        return mapping
+        return exercise_mapping
+
+    def simulate_workout_data(self, exercise_mapping: dict) -> dict:
+        """Simulate data for sets, reps and weight for given exercises.
+
+        :param exercise_mapping: A dictionary mapping exercise names to weight ranges.
+        :type exercise_mapping: dict
+        :return: A dictionary mapping exercise names to a list of sets, reps and weights.
+        :rtype: dict
+        """
+
+        workout_data = {}
+
+        for exercise_name, weight_range in exercise_mapping.items():
+            no_of_sets = random.randint(1, 6)
+            workout_data[exercise_name] = []
+            for actual_set in range(1, no_of_sets + 1):
+                actual_reps = random.randint(1, 10)
+                actual_weight = self.calculate_weight(weight_range, actual_reps)
+
+                workout_data[exercise_name].append(
+                    {
+                        "set_number": actual_set,
+                        "reps": actual_reps,
+                        "weight": actual_weight,
+                    }
+                )
+
+        return workout_data
 
     def format_data(self) -> dict:
         """Prepare data format for JSON file.
@@ -98,7 +130,7 @@ class WorkoutSimulator:
         return {
             "date": self.workout_date,
             "split": self.split,
-            "exercises": self.simulate_sets_reps_weight(),
+            "exercises": self.simulate_workout_data(),
         }
 
     def write_data(self) -> None:

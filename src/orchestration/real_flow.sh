6c061066fb79c6ea7842d4f61f0fcaad74ccc391
Gustav Rasmussen <gcr84@hotmail.com>
2023-05-07 18:25:52 +0200

    chore: add error handling to Bash script

--- src/orchestration/real_flow.sh
+++ src/orchestration/real_flow.sh
@@ -8,18 +8,33 @@
 WORKOUT_DATE=$(date +%F)  # '2023-01-19'  # 2022-03-02,2022-03-03
 # TRAINING_PROGRAM='nfp'  # 'gvt'
 
-# python3 src/CRUD/insert.py --datatype real --dates "$WORKOUT_DATE"  # --workout_number 2
-echo 'data inserted in database. Preparing figures ..'
-python3 src/combined_metrics/combined_metrics.py
-open img/workout_frequency.png
-open img/workout_duration.png
+# --workout_number 2
+if ! python3 ./src/CRUD/insert.py --datatype real --dates "$WORKOUT_DATE"; then
+    echo "Error: Failed to insert data in database."
+    exit 1
+fi
+
+echo "Data inserted in database. Preparing figures..."
+
+if ! python3 ./src/combined_metrics/combined_metrics.py; then
+    echo "Error: Failed to prepare figures."
+    exit 1
+fi
 
 # python3 src/model/plot_model.py --datatype real --pgm $TRAINING_PROGRAM
 
-# open img/real_fitted_data_squat_${TRAINING_PROGRAM}.png
-# open img/real_fitted_data_barbell_bench_press_${TRAINING_PROGRAM}.png
+open_images() {
+  open ./img/workout_frequency.png
+  open ./img/workout_duration.png
+  # open img/real_fitted_data_squat_${TRAINING_PROGRAM}.png
+  # open img/real_fitted_data_barbell_bench_press_${TRAINING_PROGRAM}.png
+  # open img/real_fitted_data_squat_splines.png
+  # open img/real_fitted_data_deadlift_splines.png
+  # open img/real_fitted_data_seated_row_splines.png
+  # open img/real_fitted_data_barbell_bench_press_splines.png
+}
 
-# open img/real_fitted_data_squat_splines.png
-# open img/real_fitted_data_deadlift_splines.png
-# open img/real_fitted_data_seated_row_splines.png
-# open img/real_fitted_data_barbell_bench_press_splines.png
+if ! open_images; then
+    echo "Error: Failed to open images."
+    exit 1
+fi

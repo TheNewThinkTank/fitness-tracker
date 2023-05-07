17d9fc323f1de5562a3200aa8a6f46e01209db14
Gustav Rasmussen <gcr84@hotmail.com>
2023-05-07 19:44:16 +0200

    chore: fix lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,13 +35,13 @@ release = "0.1.0"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-              "sphinx.ext.autodoc",
-              "sphinx.ext.mathjax",
-              'sphinx.ext.todo',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.ifconfig',
-              # "sphinx_markdown_builder",
-              ]
+            "sphinx.ext.autodoc",
+            "sphinx.ext.mathjax",
+            'sphinx.ext.todo',
+            'sphinx.ext.viewcode',
+            'sphinx.ext.ifconfig',
+            # "sphinx_markdown_builder",
+            ]
 
 mathjax_path = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
@@ -65,5 +65,5 @@ html_theme = "sphinx_rtd_theme"  # 'alabaster'  # 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = [
-                    # "_static",
-                    ]
+                   # "_static",
+                   ]

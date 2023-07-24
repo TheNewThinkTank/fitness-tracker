ee84760e93c7c4c8c1e0e2382b610170756c7e2b
Gustav Rasmussen <gcr84@hotmail.com>
2023-07-24 22:05:26 +0200

    chore: add loop through months for duration plot

--- src/orchestration/real_flow.sh
+++ src/orchestration/real_flow.sh
@@ -7,10 +7,15 @@
 # Purpose: BASH workflow that inserts data in database.
 
 WORKOUT_DATE=$(date +%F)  # '2023-01-19'  # 2022-03-02,2022-03-03
+
+YEAR_TO_PLOT="2023"
+MONTH_TO_PLOT="July"
+
 # TRAINING_PROGRAM='nfp'  # 'gvt'
 FILE_FORMAT='yml'  # default is json
 
 # --workout_number 2
+
 if ! python3 ./src/CRUD/insert.py --file_format "$FILE_FORMAT" --datatype real --dates "$WORKOUT_DATE"; then
     echo "Error: Failed to insert data in database."
     exit 1
@@ -18,7 +23,7 @@ fi
 
 echo "Data inserted in database. Preparing figures..."
 
-if ! python3 ./src/combined_metrics/combined_metrics.py --file_format "$FILE_FORMAT"; then
+if ! python3 ./src/combined_metrics/combined_metrics.py --file_format "$FILE_FORMAT" --year_to_plot "$YEAR_TO_PLOT" --month_to_plot "$MONTH_TO_PLOT"; then
     echo "Error: Failed to prepare figures."
     exit 1
 fi
@@ -27,7 +32,9 @@ fi
 
 open_images() {
   open ./img/workout_frequency.png
-  open ./img/workout_duration.png
+  # open ./img/workout_duration.png
+  open ./img/workout_duration_"$MONTH_TO_PLOT"_"$YEAR_TO_PLOT".png
+  # open ./img/workout_duration_volume_1rm_bb_bench_press.png
   # open img/real_fitted_data_squat_${TRAINING_PROGRAM}.png
   # open img/real_fitted_data_barbell_bench_press_${TRAINING_PROGRAM}.png
   # open img/real_fitted_data_squat_splines.png

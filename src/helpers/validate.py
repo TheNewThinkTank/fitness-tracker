71d2679ea166912aeaa8c3871b353d2a770fdeb9
Gustav Rasmussen <gcr84@hotmail.com>
2023-09-30 22:19:56 +0200

    test: update path to Google Drive

--- src/helpers/validate.py
+++ src/helpers/validate.py
@@ -123,7 +123,7 @@ def main() -> None:
     google_drive_data_path = (
         DATA["google_drive_data_path"]
         .replace("<USER>", "gustavcollinrasmussen")
-        .replace("<EMAIL>", "gcr84@hotmail.com")
+        # .replace("<EMAIL>", "gcr84@hotmail.com")
     )
 
     # DATA = json.load(open(file="./config.json", encoding="utf-8"))

d9d7c768c0e7e458cc1d82ef29223ef5436a4390
Gustav Rasmussen <gcr84@hotmail.com>
2024-01-19 15:41:26 +0100

    chore: add yesterday's workout

--- src/orchestration/real_flow.sh
+++ src/orchestration/real_flow.sh
@@ -12,12 +12,12 @@ FILE_FORMAT='yml'  # default is json
 
 # --workout_number 2
 
-if ! python3 ./src/CRUD/insert.py --file_format "$FILE_FORMAT" --datatype real --dates "$WORKOUT_DATE"; then
-    echo "Error: Failed to insert data in database."
-    exit 1
-fi
+# if ! python3 ./src/CRUD/insert.py --file_format "$FILE_FORMAT" --datatype real --dates "$WORKOUT_DATE"; then
+#     echo "Error: Failed to insert data in database."
+#     exit 1
+# fi
 
-echo "Data inserted in database. Preparing figures..."
+# echo "Data inserted in database. Preparing figures..."
 
 if ! python3 ./src/combined_metrics/combined_metrics.py --year_to_plot "$YEAR_TO_PLOT" --month_to_plot "$MONTH_TO_PLOT"; then
     echo "Error: Failed to prepare figures."
@@ -26,21 +26,21 @@ fi
 
 # python3 src/model/plot_model.py --datatype real --pgm $TRAINING_PROGRAM
 
-open_images() {
-  open ./img/"$YEAR_TO_PLOT"_workout_frequency.png
-  # open ./img/workout_frequency.png
-  # open ./img/workout_duration.png
-  open ./img/workout_duration_"$MONTH_TO_PLOT"_"$YEAR_TO_PLOT".png
-  # open ./img/workout_duration_volume_1rm_bb_bench_press.png
-  # open img/real_fitted_data_squat_${TRAINING_PROGRAM}.png
-  # open img/real_fitted_data_barbell_bench_press_${TRAINING_PROGRAM}.png
-  # open img/real_fitted_data_squat_splines.png
-  # open img/real_fitted_data_deadlift_splines.png
-  # open img/real_fitted_data_seated_row_splines.png
-  # open img/real_fitted_data_barbell_bench_press_splines.png
-}
+# open_images() {
+#   open ./img/"$YEAR_TO_PLOT"_workout_frequency.png
+#   # open ./img/workout_frequency.png
+#   # open ./img/workout_duration.png
+#   open ./img/workout_duration_"$MONTH_TO_PLOT"_"$YEAR_TO_PLOT".png
+#   # open ./img/workout_duration_volume_1rm_bb_bench_press.png
+#   # open img/real_fitted_data_squat_${TRAINING_PROGRAM}.png
+#   # open img/real_fitted_data_barbell_bench_press_${TRAINING_PROGRAM}.png
+#   # open img/real_fitted_data_squat_splines.png
+#   # open img/real_fitted_data_deadlift_splines.png
+#   # open img/real_fitted_data_seated_row_splines.png
+#   # open img/real_fitted_data_barbell_bench_press_splines.png
+# }
 
-if ! open_images; then
-    echo "Error: Failed to open images."
-    exit 1
-fi
+# if ! open_images; then
+#     echo "Error: Failed to open images."
+#     exit 1
+# fi

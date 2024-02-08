2cd558313b2f2d435c32aafc12ee862bfde3ae2b
Gustav Rasmussen <gcr84@hotmail.com>
2024-02-08 23:02:06 +0100

    test: nested query

--- src/CRUD/update.py
+++ src/CRUD/update.py
@@ -7,14 +7,14 @@ import os
 import sys
 
 from icecream import ic  # type: ignore
-from tinydb import Query  # type: ignore
+from tinydb import Query, where  # type: ignore
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from helpers.set_db_and_table import set_db_and_table  # type: ignore
 
 
-def update_table(table):
+def search_table(table):
     """_summary_
 
     :param table: _description_
@@ -22,24 +22,53 @@ def update_table(table):
     """
 
     Workout = Query()
-    Exercise = Query()
+    # Exercise = Query()
 
-    # groups.search(Group.permissions.any(Permission.type == 'read'))
-
+    # table.search(Workout.Exercise.any(Exercise.weight == '35 kg'))
     # data = {"weight": "BODYWEIGHT - 35 kg"}
 
-    search = table.search(
-        Workout.exercises.any(
-            Exercise.assisted_pullup.weight == "BODYWEIGHT - 35 kg"
-        )
-    )
+    # search_results = table.search(
+    #     Workout.exercises.any(
+    #         (Exercise.assisted_pullup.any(Exercise.weight == "BODYWEIGHT - 35 kg"))
+    #     )
+    # )
 
-    # table.search(Check["json-object"]["test"].exists())
+    # ic(table)
+    # q = Query()
+    # search_results = table.search(Workout.exercises.exists())
+    # search_results = table.search(Workout.exercises.weight == "BODYWEIGHT - 35 kg")
+    # search_results = table.search(where('exercises').weight == "BODYWEIGHT - 35 kg")
+    # search_results = table.search(Query().fragment({'weight': "BODYWEIGHT - 35 kg"}))
+    search_results = table.search(Workout.exercises.search('BODYWEIGHT'))
+
+    # search_results = table.search(
+    #     Workout.exercises.any(
+    #         Workout.assisted_pullup.exists()
+    #     )
+    #     )
+
+    # search_results = table.search(
+    #     q.exercises.any(
+    #         q.assisted_pullup.exists()
+    #     )
+    # )
+
+    # search_results = table.search(
+    #     q.exercises.any(
+    #         q.assisted_pullup.any(q.weight == "BODYWEIGHT - 35 kg")
+    #     )
+    # )
+
+    # search_results = table.search(
+    #     q.exercises.any(
+    #         q.assisted_pullup.any(q.weight.exists())
+    #     )
+    # )
     # search = table.search(Workout.exercises.Exercise.fragment(data))
 
     # table.update({"reps": 10}, Item.exercises.chinup.reps == 6)
 
-    return search
+    return search_results
 
 
 def remove_from_table(table) -> None:
@@ -77,7 +106,7 @@ def main() -> None:
     # ic(db)
     # ic(table)
 
-    print(update_table(table))
+    print(search_table(table))
     # remove_from_table(table)
     # truncate_table(table)
 

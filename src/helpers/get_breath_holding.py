c5bf5ece9260c4ff6d1dbf7f85ac88ddc92dc953
Gustav Rasmussen <gcr84@hotmail.com>
2024-07-14 14:05:01 +0200

    chore: add todays breath holding

--- src/helpers/get_breath_holding.py
+++ src/helpers/get_breath_holding.py
@@ -13,35 +13,39 @@ def get_breath_holding():
     :rtype: _type_
     """
 
-    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRqzqJPhFMBBrMw3710Q1Ws2eUTqVDUTpNdXqxneW2otr_4xfgVYWLxrIH8NDJOeBbs8Pq4ZLs76eEi/pub?output=csv"
+    base_url = "https://docs.google.com/spreadsheets"
+    url = base_url + "/d/e/2PACX-1vRqzqJPhFMBBrMw3710Q1Ws2eUTqVDUTpNdXqxneW2otr_4xfgVYWLxrIH8NDJOeBbs8Pq4ZLs76eEi/pub?output=csv"
+
     df = pd.read_csv(url)
 
     cols = [
         "DATE",
         "SET-NUMBER",
         "START-TIME (HH:MM)",
-        "TIMEZONE",
-        "DURATION (HH:MM)",
+        "DURATION (MM:SS)",
         "FROM-INHALE",
         "CONTROLLED-HYPERVENTILATION",
+        "TIMEZONE",
+        "LOCATION",
         "NOTES",
     ]
 
-    return df[["DATE", "SET-NUMBER", "DURATION (HH:MM)"]].dropna()  # df[""].values[-1]
+    return df[["DATE", "SET-NUMBER", "DURATION (MM:SS)"]].dropna()  # df[""].values[-1]
 
 
 def main():
     df = get_breath_holding()
 
     # Convert 'DURATION (HH:MM)' to seconds
-    df['DURATION (HH:MM)'] = df['DURATION (HH:MM)'].apply(lambda x: int(x.split(':')[0]) * 60 + int(x.split(':')[1]))
+    df['DURATION (MM:SS)'] = df['DURATION (MM:SS)'].apply(lambda x: int(x.split(':')[0]) * 60 + int(x.split(':')[1]))
 
     # Rename column for clarity
-    df = df.rename(columns={'DURATION (HH:MM)': 'DURATION (Seconds)'})
+    df = df.rename(columns={'DURATION (MM:SS)': 'DURATION (Seconds)'})
 
     # Create Seaborn barplot
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x='SET-NUMBER', y='DURATION (Seconds)', data=df)
+    plt.figure(figsize=(12, 6))
+    # sns.barplot(x='SET-NUMBER', y='DURATION (Seconds)', data=df)
+    sns.barplot(x='SET-NUMBER', y='DURATION (Seconds)', hue='DATE', data=df)
 
     # Add labels and title
     plt.xlabel('Set Number')
@@ -50,7 +54,7 @@ def main():
 
     # Show plot
     # plt.show()
-    plt.savefig("docs/project_docs/img/breathholding/2024-07-13.png")
+    plt.savefig("docs/project_docs/img/breathholding/2024-07-14.png")
 
 
 if __name__ == "__main__":

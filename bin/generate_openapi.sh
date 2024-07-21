328e57fc18a45577847b56e4e08aef95549ca700
Gustav Rasmussen <gcr84@hotmail.com>
2024-07-21 12:24:33 +0200

    test: GitHub Actions wf to update repo with OpenAPI JSON

---
+++ bin/generate_openapi.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Start the FastAPI application
+uvicorn src.main:app --host 127.0.0.1 --port 8000 &
+# uvicorn src.main:app --host 0.0.0.0 --port 80
+PID=$!
+sleep 5  # Give the server time to start
+
+# Ensure the directory exists
+mkdir -p docs/schema
+
+# Fetch the OpenAPI JSON and format it
+curl http://127.0.0.1:8000/openapi.json | jq . > docs/schema/openapi.json
+
+# Kill the server
+kill $PID

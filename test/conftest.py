5e38c47ef3f6591348506b636d4d1f2e12670c3b
Gustav Rasmussen <gcr84@hotmail.com>
2024-08-22 21:28:42 +0200

    test: append ../src/crud to PYTHONPATH

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/crud')))
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))

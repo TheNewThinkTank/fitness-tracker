30c6c242efef618febc341e199df39157abdfc75
Gustav Rasmussen <gcr84@hotmail.com>
2024-08-22 21:23:05 +0200

    test: append src to PYTHONPATH

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))

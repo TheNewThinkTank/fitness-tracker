8b78045db01dc597700e84f048a8af977ca185ce
Gustav Rasmussen <gcr84@hotmail.com>
2024-08-22 22:33:27 +0200

    test: add python-dotenv

--- test/test_validate.py
+++ test/test_validate.py
@@ -1,17 +1,41 @@
-import json
+import os
+import yaml
 from pprint import pprint as pp
 
 from test.conftest import src
 from src.helpers.validate import Workout
 
+from dotenv import load_dotenv
+
+load_dotenv()
+email = os.environ["EMAIL"]
+
 
 def setup():
     """Read data from a JSON file."""
 
-    DATA = json.load(open(file="./config.json", encoding="utf-8"))
-    file = DATA["real_workout_database"].replace("<ATHLETE>", "gustav_rasmussen")
+    # DATA = json.load(open(file="./config.json", encoding="utf-8"))
+    with open("./.config/config.yml", "r") as rf:
+        DATA = yaml.safe_load(rf)
+
+    user = "gustavcollinrasmussen"
+    athlete = "gustav_rasmussen"
+
+    google_drive_data_path = (
+        DATA["google_drive_data_path"]
+        .replace("<USER>", user)
+        .replace("<EMAIL>", email)
+    )
+
+    file = (
+        DATA["real_workout_database"]
+        .replace("<GOOGLE_DRIVE_DATA_PATH>", google_drive_data_path)
+        .replace("<ATHLETE>", athlete)
+        .replace("<YEAR>", "2024")
+    )
+
     with open(file) as rf:
-        data = json.load(rf)["weight_training_log"]
+        data = yaml.safe_load(rf)["weight_training_log"]
     return data
 
 

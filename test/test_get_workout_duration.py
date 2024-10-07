8de7f89005fc440c83ca22c350c99f74780d028b
Gustav Rasmussen <gcr84@hotmail.com>
2024-10-07 22:36:47 +0200

    fix: encapsulate globals

--- test/test_get_workout_duration.py
+++ test/test_get_workout_duration.py
@@ -1,8 +1,16 @@
 from test.conftest import src
 from src.helpers.get_workout_duration import get_workout_duration
+from src.helpers.config_loader import ConfigLoader  # type: ignore
 
 
 def test_get_year_and_week():
+
+    # env_vars = ConfigLoader.load_env_variables()
+    # config = ConfigLoader.load_config(
+    #     athlete=env_vars["athlete"],
+    #     user=env_vars["user"],
+    #     email=env_vars["email"],
+    # )
 
     assert (
         get_workout_duration("14:45", "15:10") == 25

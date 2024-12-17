40a9d7bd7906ae5e66d333babae0a1adcc87b3db
Gustav Rasmussen <gcr84@hotmail.com>
2024-12-17 19:57:04 +0100

    refactor: move simulate_people to algo-library package. Update README. Upgrade package versions

--- src/simulations/simulate_people.py
+++ src/simulations/simulate_people.py
@@ -1,39 +1,6 @@
 """Generate fictitious people."""
 
-import os
-from pathlib import Path
-from faker import Faker
-
-
-def generate_person(fake, data_dir:str = "data") -> str:
-    """_summary_
-
-    :return: _description_
-    :rtype: str
-    """
-
-    person = fake.name().replace(" ", "_").lower()
-    print(person)
-    Path(os.path.join(data_dir, person)).mkdir(parents=True, exist_ok=True)
-
-    person_dir = os.path.join(data_dir, person)
-    if not os.listdir(person_dir):
-        print(f"{person_dir} is empty! Creating .gitkeep ...")
-        with open(os.path.join(person_dir, ".gitkeep"), "w", encoding="utf8") as f:
-            f.write("")
-
-    return person
-
-
-def generate_people(locale:str="en_US", num_people:int=2) -> None:
-    """_summary_
-    """
-
-    fake = Faker(locale)  # locale = "it_IT" # default: "en_US"
-
-    for _ in range(num_people):
-        generate_person(fake)
-
+from algo_lib.simulation.simulate_people import generate_people  # type: ignore
 
 if __name__ == "__main__":
-    generate_people()
+    generate_people(num_people=3)

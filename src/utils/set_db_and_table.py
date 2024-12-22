5afafa77c829ed57bbcf50ec022dc9578eeb7383
Gustav Rasmussen <gcr84@hotmail.com>
2024-12-22 23:11:38 +0100

    test: update path for github runner

--- src/utils/set_db_and_table.py
+++ src/utils/set_db_and_table.py
@@ -57,11 +57,20 @@ def set_db_and_table(
         training_catalogue = "src/utils/muscles_and_exercises.yaml"
         return db, table, training_catalogue
 
-    db_path = (
-        config["real_workout_database"]
-        .replace("<ATHLETE>", athlete)
-        .replace("<YEAR>", str(year))
-    ) if datatype == "real" else config["simulated_workout_database"]
+    workspace = os.getenv('GITHUB_WORKSPACE', default='.')
+    # db_path = os.path.join(workspace, f"data/{year}_workouts.yml") if datatype == "real" else os.path.join(workspace, "data/simulated_workouts.yml")
+
+    # db_path = f"data/{year}_workouts.yml" if datatype == "real" else f"data/simulated_workouts.yml"
+
+    # db_path = (
+    #     config["real_workout_database"]
+    #     .replace("<YEAR>", str(year))
+    # ) if datatype == "real" else config["simulated_workout_database"]
+
+    if datatype == "real":
+        db_path = os.path.join(workspace, config["real_workout_database"].replace("<YEAR>", str(year)))
+    else:
+        db_path = os.path.join(workspace, config["simulated_workout_database"])
 
     db_singleton = TinyDBSingleton(db_path)
     db = db_singleton.get_db()

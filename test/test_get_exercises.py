eb244b7a8afc55a8d555bcbf11e8baa3f4925171
Gustav Rasmussen <gcr84@hotmail.com>
2024-12-22 22:30:39 +0100

    test: add test_get_available_exercises

--- test/test_get_exercises.py
+++ test/test_get_exercises.py
@@ -1,16 +1,14 @@
 
-import pytest
-
+# import pytest
 from test.conftest import src
 from src.utils.get_exercises import get_available_exercises
 from src.utils.set_db_and_table import set_db_and_table  # type: ignore
 
 
-@pytest.mark.skip(reason="Skip until PermissionError is fixed")
+# @pytest.mark.skip(reason="Skip until PermissionError is fixed")
 def test_get_available_exercises():
 
     _, _, training_catalogue = set_db_and_table(datatype="real")
-
     available_exercises = get_available_exercises(training_catalogue, "back")
 
     assert available_exercises == [

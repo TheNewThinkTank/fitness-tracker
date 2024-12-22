a5834f1cdde769081d4cabc017dcb4cc29634379
Gustav Rasmussen <gcr84@hotmail.com>
2024-12-22 23:48:04 +0100

    test: add unit tests

--- test/test_update.py
+++ test/test_update.py
@@ -1,24 +1,24 @@
-# # TODO: fix import
-# from tinydb import TinyDB
-# from test.conftest import src
-# from src.crud.update import (  # type: ignore
-#     # filter_exercises_with_whitespace,
-#     clean_exercise_name,
-#     # clean_exercise_names
-# )
+# TODO: fix import
+from tinydb import TinyDB
+from test.conftest import src
+from src.crud.update import (  # type: ignore
+    # filter_exercises_with_whitespace,
+    clean_exercise_name,
+    # clean_exercise_names
+)
 
 
-# def setup():
-#     MOCK_DB = TinyDB("test/test_db.json")
-#     test_table = MOCK_DB.table("test_log")
-#     return test_table
+def setup():
+    MOCK_DB = TinyDB("test/test_db.json")
+    test_table = MOCK_DB.table("test_log")
+    return test_table
 
 
-# def test_clean_exercise_name():
-#     assert clean_exercise_name("bench press") == "bench_press"
-#     assert clean_exercise_name("bench  press") == "bench_press"
-#     assert clean_exercise_name("bench_press") == "bench_press"
-#     assert clean_exercise_name("bench_ press") == "bench_press"
-#     assert clean_exercise_name("bench_  press") == "bench_press"
-#     assert clean_exercise_name("bench  _  press") == "bench_press"
-#     assert clean_exercise_name("shoulder press_") == "shoulder_press"
+def test_clean_exercise_name():
+    assert clean_exercise_name("bench press") == "bench_press"
+    assert clean_exercise_name("bench  press") == "bench_press"
+    assert clean_exercise_name("bench_press") == "bench_press"
+    assert clean_exercise_name("bench_ press") == "bench_press"
+    assert clean_exercise_name("bench_  press") == "bench_press"
+    assert clean_exercise_name("bench  _  press") == "bench_press"
+    assert clean_exercise_name("shoulder press_") == "shoulder_press"

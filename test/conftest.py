9d8e5599567fc66d607304943ea3356a98d12a95
Gustav Rasmussen <gcr84@hotmail.com>
2024-12-23 00:01:31 +0100

    test: update conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,19 +1,19 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/crud')))
 
 # Determine the absolute path of the root directory and src directory
-# ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-# SRC_DIR = os.path.join(ROOT_DIR, 'src')
-# # Add the src directory to sys.path
-# sys.path.insert(0, SRC_DIR)
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+SRC_DIR = os.path.join(ROOT_DIR, 'src')
+# Add the src directory to sys.path
+sys.path.insert(0, SRC_DIR)
 
 # SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 # sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-# import src
+import src

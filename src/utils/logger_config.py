febbd61b2a0befdba8801d210b65da8153cb4f1b
Gustav Rasmussen <gcr84@hotmail.com>
2024-12-25 12:49:18 +0100

    refactor: split get_pgm_from_date up in 3. Add docstrings

--- src/utils/logger_config.py
+++ src/utils/logger_config.py
@@ -1,8 +1,18 @@
+"""Logging configuration for the project.
+"""
+
 import logging
 from pathlib import Path
 
 
-def setup_logger(log_file: str = "insert.log", log_dir: str = "logs/"):
+def setup_logger(log_file: str = "insert.log", log_dir: str = "logs/") -> None:
+    """_summary_
+
+    :param log_file: _description_, defaults to "insert.log"
+    :type log_file: str, optional
+    :param log_dir: _description_, defaults to "logs/"
+    :type log_dir: str, optional
+    """
     # Create the log directory if it doesn't exist
     Path(log_dir).mkdir(parents=True, exist_ok=True)
 
@@ -26,6 +36,6 @@ def setup_logger(log_file: str = "insert.log", log_dir: str = "logs/"):
     logging.info("Logger initialized")
 
 
-def log_running_file(file_path):
+def log_running_file(file_path: str) -> None:
     """Optional utility for logging the running file."""
     logging.info("Running %s ...", "/".join(file_path.split("/")[-4:]))

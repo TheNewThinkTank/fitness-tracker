7aa37fb6edd8429cb48aa87cb509b9d0797b9f76
Gustav Rasmussen <gcr84@hotmail.com>
2025-01-05 18:11:04 +0100

    chore: remove dist

--- bin/repo_cleaner.sh
+++ bin/repo_cleaner.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# show the sizes of all top-level directories
+# du -sh ./* | sort -h
+
+# sizes of all directories recursively
+# du -h --max-depth=1 | sort -h
+
 # find large files in the git history
 find_large_files() {
   git rev-list --objects --all | \
@@ -10,3 +16,11 @@ find_large_files() {
 }
 
 find_large_files
+
+git rm -r --cached node_modules/
+git rm -r --cached public/build/
+git rm -r --cached .svelte-kit/
+git rm --cached .env
+git rm -r --cached .vscode/
+git rm -r --cached .idea/
+git rm --cached .DS_Store

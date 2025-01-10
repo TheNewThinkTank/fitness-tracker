d60791db93f1e6ca41e477dd637a181be8deb5cf
Gustav Rasmussen <gcr84@hotmail.com>
2025-01-10 14:51:07 +0100

    fix: double quote to prevent globbing and word splitting

--- bin/fitcli.sh
+++ bin/fitcli.sh
@@ -124,8 +124,8 @@ main() {
   if [ -f .config/config.yml ]; then
     GOOGLE_DRIVE_DATA_PATH=$(yq e '.google_drive_data_path' .config/config.yml | sed "s/<USER>/$USER/g; s/<EMAIL>/$EMAIL/g")
     IMG_PATH=$(yq e '.img_path' .config/config.yml | sed "s|<GOOGLE_DRIVE_DATA_PATH>|$GOOGLE_DRIVE_DATA_PATH|g; s/<ATHLETE>/$ATHLETE/g")
-    echo $GOOGLE_DRIVE_DATA_PATH  # /Users/$USER/Library/CloudStorage/GoogleDrive-$EMAIL/My Drive/DATA/fitness-tracker-data
-    echo $IMG_PATH  # $GOOGLE_DRIVE_DATA_PATH/$ATHLETE/img/
+    echo "$GOOGLE_DRIVE_DATA_PATH"  # /Users/$USER/Library/CloudStorage/GoogleDrive-$EMAIL/My Drive/DATA/fitness-tracker-data
+    echo "$IMG_PATH"  # $GOOGLE_DRIVE_DATA_PATH/$ATHLETE/img/
     IMG_PATH="${IMG_PATH}/${YEAR_TO_PLOT}"
   else
     echo "Warning: config.yml file not found. Using default img_path."
--- bin/test_cli.sh
+++ bin/test_cli.sh
@@ -17,8 +17,8 @@ fi
 if [ -f .config/config.yml ]; then
   GOOGLE_DRIVE_DATA_PATH=$(yq e '.google_drive_data_path' .config/config.yml | sed "s/<USER>/$USER/g; s/<EMAIL>/$EMAIL/g")
   IMG_PATH=$(yq e '.img_path' .config/config.yml | sed "s|<GOOGLE_DRIVE_DATA_PATH>|$GOOGLE_DRIVE_DATA_PATH|g; s/<ATHLETE>/$ATHLETE/g")
-  echo $GOOGLE_DRIVE_DATA_PATH  # /Users/$USER/Library/CloudStorage/GoogleDrive-$EMAIL/My Drive/DATA/fitness-tracker-data
-  echo $IMG_PATH  # $GOOGLE_DRIVE_DATA_PATH/$ATHLETE/img/
+  echo "$GOOGLE_DRIVE_DATA_PATH"  # /Users/$USER/Library/CloudStorage/GoogleDrive-$EMAIL/My Drive/DATA/fitness-tracker-data
+  echo "$IMG_PATH"  # $GOOGLE_DRIVE_DATA_PATH/$ATHLETE/img/
 else
   echo "Warning: config.yml file not found. Using default img_path."
   IMG_PATH="/Users/${USER}/Library/CloudStorage/GoogleDrive-${EMAIL}/My Drive/DATA/fitness-tracker-data/${ATHLETE}/img/2025/"

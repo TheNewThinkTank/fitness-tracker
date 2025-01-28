782915da28a74aacb2b77e1e80326c3efa056820
Gustav Rasmussen <gcr84@hotmail.com>
2025-01-28 09:52:28 +0100

    chore: test for GITHUB_ACTIONS env var

--- src/utils/set_db_and_table.py
+++ src/utils/set_db_and_table.py
@@ -57,7 +57,10 @@ def set_db_and_table(
     """
 
     if not athlete:
-        athlete = config_data["ATHLETE"]
+        if 'GITHUB_ACTIONS' in os.environ:
+            athlete = config_data["athlete"]
+        else:
+            athlete = config_data["ATHLETE"]
 
     if not year:
         year = datetime.now().year

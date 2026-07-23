c375e70088bb4c38f8fa907d46f1158c62294348
Gustav Rasmussen <TheNewThinkTank@users.noreply.github.com>
2026-07-23 17:10:35 -0400

    fix: remove file-convertion-tools for now

--- src/utils/cleanup.py
+++ src/utils/cleanup.py
@@ -3,18 +3,20 @@ Empty specified folder or file.
 """
 
 import sys
-from file_convertion_tools.cleanup import cleanup, empty_file  # type: ignore
+# from file_convertion_tools.cleanup import cleanup, empty_file  # type: ignore
 
 
 def main() -> None:
     """Empty specified folder or file.
     """
 
-    path: str = sys.argv[1]
-    file_path: str = sys.argv[2]
+    # path: str = sys.argv[1]
+    # file_path: str = sys.argv[2]
 
-    cleanup(path)
-    empty_file(file_path)
+    # cleanup(path)
+    # empty_file(file_path)
+
+    pass
 
 
 if __name__ == "__main__":
--- src/utils/file_conversions/json_to_yaml.py
+++ src/utils/file_conversions/json_to_yaml.py
@@ -3,7 +3,7 @@
 
 from pathlib import Path
 from src.utils.config import settings  # type: ignore
-from file_convertion_tools.json_to_yaml import json_to_yaml  # type: ignore
+# from file_convertion_tools.json_to_yaml import json_to_yaml  # type: ignore
 # from src.utils.config import config_data  # type: ignore
 
 
@@ -11,11 +11,14 @@ def main() -> None:
     """Convert a JSON file to a YAML file.
     """
 
-    in_file_name = "/db.json"  # f"/log_archive/JSON/2023/June/test.json"
-    in_file = f'{settings["GOOGLE_DRIVE_DATA_PATH"]}/{settings["ATHLETE"]}{in_file_name}'
+    # in_file_name = "/db.json"  # f"/log_archive/JSON/2023/June/test.json"
+    # in_file = f'{settings["GOOGLE_DRIVE_DATA_PATH"]}/{settings["ATHLETE"]}{in_file_name}'
 
-    json_to_yaml(Path(in_file))
+    # json_to_yaml(Path(in_file))
+
+    pass
 
 
 if __name__ == "__main__":
     main()
+
